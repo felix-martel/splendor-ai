acfa61c59c13aed1114d11f8e55667143d6fc786
felix <felix.martel@polytechnique.edu>
2018-03-04 17:48:31 +0100

    Agent and RandomAgent

--- main_agent.py
+++ main_agent.py
@@ -1,22 +1,42 @@
 from Environment import Environment
+from RandomAgent import RandomAgent
 
 # Constants
 n_games = 10
 max_step = 1000
 last_debug = {}
+
+# Init game environment
 game = Environment()
+
+# Start new game
 player = game.get_player()
+initial_state = game.state.visible()
+initial_actions = game.get_possible_actions(player)
 
-print(player)
+# Init agent
+agent = RandomAgent()
+agent.new_game(player)
+agent.observe(initial_state, 0, False, initial_actions)
 
+# Start playing !
 t = 0
+reward = 0
 game_ended = False
 while not game_ended and t < max_step:
-    action = game.get_random_action(player)
+    # -- Beginning of our turn --
+    # Observe current state
+    state = game.state.visible()
+    actions = game.get_possible_actions(agent.identity)    
+    agent.observe(state, reward, game_ended, actions)
+    # Take action
+    action = agent.act()    
+    state, reward, game_ended, _debug = game.take_action(action, agent.identity)
+    # -- End of our turn --
     
-    state, reward, game_ended, _debug = game.take_action(action, player)
     last_debug = _debug
     t += 1
+    # Other players' turn
     game.autoplay()
 
 if game_ended:
